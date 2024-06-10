b4aa3c3311fbf3779e3c09266cb3dc7be4a88a11
Diogo Braz <diogo.braz04@gmail.com>
2024-06-10 17:35:48 -0300

    refactor: move LoadFacebookUserApi to its own file

---
+++ src/data/contracts/apis/facebook.ts
@@ -0,0 +1,9 @@
+export interface LoadFacebookUserApi {
+  loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>;
+}
+
+export namespace LoadFacebookUserApi {
+  export type Params = { token: string };
+
+  export type Result = undefined;
+}
