a775cdf10066dbe60aabf2d8baaae2f0118adf4a
Diogo Braz <diogo.braz04@gmail.com>
2024-06-11 05:57:05 -0300

    feat: ensure FacebookApi gets debug token

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -11,12 +11,21 @@ export class FacebookApi {
   ) {}
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
         client_secret: this.clientSecret,
         grant_type: "client_credentials"
+      }
+    });
+    await this.httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
+      params: {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        access_token: appToken.access_token,
+        input_token: params.token
       }
     });
   }
