b53753e9913e71b245b714645232eb9973a878e3
Diogo Braz <diogo.braz04@gmail.com>
2024-06-12 18:51:15 -0300

    refactor: move inMemoryDb creation to its own file

--- tests/infra/postgres/repositories/user-account.spec.ts
+++ tests/infra/postgres/repositories/user-account.spec.ts
@@ -3,36 +3,7 @@ import { DataSource, Repository } from "typeorm";
 
 import { PgUser } from "@/infra/postgres/entities";
 import { PgUserAccountRepository } from "@/infra/postgres/repositories";
-
-const makeFakeDb = async (entities?: any): Promise<{ db: IMemoryDb, dataSource: DataSource }> => {
-  const db = newDb();
-  db.public.registerFunction({
-    implementation: () => "test",
-    name: "current_database"
-  });
-  db.public.registerFunction({
-    implementation: () => "version",
-    name: "version"
-  });
-  db.public.interceptQueries((queryText) => {
-    if (
-      queryText.search(
-        /(pg_views|pg_matviews|pg_tables|pg_enum|table_schema)/g
-      ) > -1
-    ) {
-      return [];
-    }
-    return null;
-  });
-  const dataSource: DataSource = await db.adapters.createTypeormDataSource({
-    type: "postgres",
-    entities: entities ?? ["src/infra/postgres/entities/index.ts"]
-  });
-  await dataSource.initialize();
-  await dataSource.synchronize();
-
-  return { db, dataSource };
-};
+import { makeFakeDb } from "@/tests/infra/postgres/mocks";
 
 describe("PgUserAccountRepository", () => {
   describe("load", () => {
