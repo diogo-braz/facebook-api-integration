0f12b554043dcf95c21b3e5b1b5e739cdc796b4e
Diogo Braz <diogo.braz04@gmail.com>
2024-06-13 02:32:08 -0300

    refactor: move production code to their own files

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,58 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { MockProxy, mock } from "jest-mock-extended";
 
 import { FacebookAuthentication } from "@/domain/features";
 import { AuthenticationError } from "@/domain/errors";
 import { AccessToken } from "@/domain/models";
-
-class FacebookLoginController {
-  constructor (
-    private readonly facebookAuthentication: FacebookAuthentication
-  ) {}
-
-  async handle (httpRequest: any): Promise<HttpResponse> {
-    try {
-      if (httpRequest.token === "" || httpRequest.token === null || httpRequest.token === undefined) {
-        return {
-          statusCode: 400,
-          data: new Error("The field token is required!")
-        };
-      }
-      const result = await this.facebookAuthentication.perform({ token: httpRequest.token });
-      if (result instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: result.value
-          }
-        };
-      } else {
-        return {
-          statusCode: 401,
-          data: result
-        };
-      }
-    } catch (error) {
-      return {
-        statusCode: 500,
-        data: new ServerError(error as Error)
-      };
-    }
-  }
-}
-
-type HttpResponse = {
-  statusCode: number,
-  data: any
-};
-
-class ServerError extends Error {
-  constructor (error?: Error) {
-    super("Server failed. Try again soon!");
-    this.name = "ServerError";
-    this.stack = error?.stack;
-  }
-}
+import { ServerError } from "@/application/errors";
+import { FacebookLoginController } from "@/application/controllers";
 
 describe("FacebookLoginController", () => {
   let facebookAuthentication: MockProxy<FacebookAuthentication>;
