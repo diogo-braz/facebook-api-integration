db1b7f19236b9bd3774ff6696b3fa1beb2c14c82
Diogo Braz <diogo.braz04@gmail.com>
2024-06-14 09:12:21 -0300

    refactor: use generics instead of any

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,15 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { HttpResponse, badRequest, ok, serverError, unauthorized } from "@/application/helpers";
 import { RequiredFieldError } from "@/application/errors";
 import { FacebookAuthentication } from "@/domain/features";
 import { AccessToken } from "@/domain/models";
+
+type HttpRequest = {
+  token: string | undefined | null
+};
+
+type Model = Error | {
+  accessToken: string
+};
 
 export class FacebookLoginController {
   constructor (
     private readonly facebookAuthentication: FacebookAuthentication
   ) {}
 
-  async handle (httpRequest: any): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       if (httpRequest.token === "" || httpRequest.token === null || httpRequest.token === undefined) {
         return badRequest(new RequiredFieldError("token"));
