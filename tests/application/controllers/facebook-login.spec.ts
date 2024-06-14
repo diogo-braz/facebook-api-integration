19c0317c4f81077669e151adbb2707fa378c835d
Diogo Braz <diogo.braz04@gmail.com>
2024-06-14 09:04:04 -0300

    refactor: apply unauthorized helper

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -3,7 +3,7 @@ import { MockProxy, mock } from "jest-mock-extended";
 import { FacebookAuthentication } from "@/domain/features";
 import { AuthenticationError } from "@/domain/errors";
 import { AccessToken } from "@/domain/models";
-import { RequiredFieldError, ServerError } from "@/application/errors";
+import { RequiredFieldError, ServerError, UnauthorizedError } from "@/application/errors";
 import { FacebookLoginController } from "@/application/controllers";
 
 describe("FacebookLoginController", () => {
@@ -61,7 +61,7 @@ describe("FacebookLoginController", () => {
 
     expect(httpResponse).toEqual({
       statusCode: 401,
-      data: new AuthenticationError()
+      data: new UnauthorizedError()
     });
   });
 
