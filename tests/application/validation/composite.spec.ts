f239c408c635f40b553ce3dcc5dfb578ba9321af
Diogo Braz <diogo.braz04@gmail.com>
2024-06-14 14:07:02 -0300

    refactor: move production code to their own files

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,23 +1,6 @@
 import { MockProxy, mock } from "jest-mock-extended";
 
-interface Validator {
-  validate: () => Error | undefined
-}
-
-class ValidationComposite implements Validator {
-  constructor (
-    private readonly validators: Validator[]
-  ) {}
-
-  validate (): Error | undefined {
-    for (const validator of this.validators) {
-      const error = validator.validate();
-      if (error !== undefined) {
-        return error;
-      }
-    }
-  }
-}
+import { ValidationComposite, Validator } from "@/application/validation";
 
 describe("ValidationComposite", () => {
   let sut: ValidationComposite;
