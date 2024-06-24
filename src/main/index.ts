82c22a42a9a8c3cbde29b320b54ea1e54331ffaf
Diogo Braz <diogo.braz04@gmail.com>
2024-06-24 16:13:42 -0300

    fix: add module alias import

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,6 @@
-import app from "./config/app";
-import { env } from "./config/env";
+import "module-alias/register";
+
+import app from "@/main/config/app";
+import { env } from "@/main/config/env";
 
 app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
