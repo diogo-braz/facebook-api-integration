719d03cd29598eaa7515411280e47a3803ca60d8
Diogo Braz <diogo.braz04@gmail.com>
2024-06-24 03:49:06 -0300

    refactor: transform ExpressRouter to a function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,19 +1,15 @@
 import { Controller } from "@/application/controllers";
 
-import { Request, Response } from "express";
+import { RequestHandler } from "express";
 
-export class ExpressRouter {
-  constructor (
-    private readonly controller: Controller
-  ) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body });
+export const adaptExpressRoute = (controller: Controller): RequestHandler => {
+  return (async (req, res) => {
+    const httpResponse = await controller.handle({ ...req.body });
     if (httpResponse.statusCode === 200) {
       res.status(200).json(httpResponse.data);
     } else {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       res.status(httpResponse.statusCode).json({ error: httpResponse.data.message });
     }
-  }
-}
+  }) as RequestHandler;
+};
