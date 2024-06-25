2bf37771cdfba899c61b841e51a6ad50e856e9bf
Diogo Braz <diogo.braz04@gmail.com>
2024-06-25 06:14:04 -0300

    refactor: rename JwtTokenGenerator to JwtTokenHandler

--- src/main/factories/use-cases/facebook-authentication.ts
+++ src/main/factories/use-cases/facebook-authentication.ts
@@ -1,12 +1,12 @@
 import { FacebookAuthentication, setupFacebookAuthentication } from "@/domain/use-cases";
 import { makeFacebookApi } from "@/main/factories/apis";
 import { makePrismaUserAccountRepository } from "@/main/factories/repositories";
-import { makeJwtTokenGenerator } from "@/main/factories/crypto";
+import { makeJwtTokenHandler } from "@/main/factories/crypto";
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePrismaUserAccountRepository(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   );
 };
